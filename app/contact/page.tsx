0affb496f25c863fd9b6209a80cb036217c54cc9
tom <lim1017@hotmail.com>
2023-04-03 14:12:28 -0400

    setup routes

---
+++ app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default Contact;
